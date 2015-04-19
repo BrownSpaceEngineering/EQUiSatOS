4561ec639c3c76e8b5e6e9a398adda2947c9f9c2
dpcollin <daniel_collins@brown.edu>
2015-04-19 13:48:33 -0400

    I2C Magnetometer refactoring
    refactored the I2C_BASIC to easily be adapted for use with other I2C
    sensors, did cleanup, added comments.  Magnetometer still probably
    broken.

--- I2C_BASIC/I2C_QUICK_START_MASTER_BASIC_USE1/src/qs_i2c_master_basic_use.c
+++ I2C_BASIC/I2C_QUICK_START_MASTER_BASIC_USE1/src/qs_i2c_master_basic_use.c
@@ -47,65 +47,73 @@
 #include <asf.h>
 #include <conf_uart_serial.h>
 
+#define HMC5883L_ADDRESS 0x1E
+#define TIMEOUT 1000
+
+
 static struct usart_module cdc_uart_module;
-void write_command(struct i2c_master_packet* packet_address);
-void read_command(struct i2c_master_packet* packet_address);
-/**
- *  Configure UART console.
- */
-static void configure_console(void) {
-    struct usart_config usart_conf;
+struct i2c_master_module i2c_master_instance;
+static void configure_console(void);
+void configure_i2c_master(void);
+void i2c_write_command(struct i2c_master_packet* packet_address);
+void i2c_read_command(struct i2c_master_packet* packet_address);
+void HMC5883L_init(void);
+void HMC5883L_read(uint8_t* read_buffer);
 
-    usart_get_config_defaults(&usart_conf);
-    usart_conf.mux_setting = CONF_STDIO_MUX_SETTING;
-    usart_conf.pinmux_pad0 = CONF_STDIO_PINMUX_PAD0;
-    usart_conf.pinmux_pad1 = CONF_STDIO_PINMUX_PAD1;
-    usart_conf.pinmux_pad2 = CONF_STDIO_PINMUX_PAD2;
-    usart_conf.pinmux_pad3 = CONF_STDIO_PINMUX_PAD3;
-    usart_conf.baudrate    = CONF_STDIO_BAUDRATE;
+int main(void){
+	//initialize SAMD21
+	system_init();
+	
+	//configure PUTTY terminal
+	configure_console();
+	printf("Running...\n\r");
+	
+	//configure i2c master on SAMD21
+	configure_i2c_master();
+	printf("I2C Configured\n\r");
+	
+	//initialize HMC5883L magnetometer with standard settings
+	HMC5883L_init();
+	printf("HMC5883L (magnetometer) initialized\n\r");	
+	
+	//Establish buffer to read from
+	static uint8_t HMC5883L_read_buffer[7] = {
+		'a','a','a','a','a','a',0x0
+	};
+	
+	//take 10 measurements
+	int i = 0;
+	while(i < 10){
+		HMC5883L_read(HMC5883L_read_buffer);
+		printf("measurement\r\n");
+		printf("%x%x%x%x%x%x\r\n",(char)HMC5883L_read_buffer[0],(char)HMC5883L_read_buffer[0],(char)HMC5883L_read_buffer[0],(char)HMC5883L_read_buffer[0],(char)HMC5883L_read_buffer[0],(char)HMC5883L_read_buffer[0]);
+		i++;
+	}
 
-    stdio_serial_init(&cdc_uart_module, CONF_STDIO_USART_MODULE,    
-&usart_conf);
-    usart_enable(&cdc_uart_module);
 }
 
+/*
+	Configure UART console. (Terminal accessable through PUTTY)
+ */
+static void configure_console(void) {
+	struct usart_config usart_conf;
 
-//! [packet_data]
-static uint8_t write_buffer_1[2] = {
-	 0x00, 0x70
-};
+	usart_get_config_defaults(&usart_conf);
+	usart_conf.mux_setting = CONF_STDIO_MUX_SETTING;
+	usart_conf.pinmux_pad0 = CONF_STDIO_PINMUX_PAD0;
+	usart_conf.pinmux_pad1 = CONF_STDIO_PINMUX_PAD1;
+	usart_conf.pinmux_pad2 = CONF_STDIO_PINMUX_PAD2;
+	usart_conf.pinmux_pad3 = CONF_STDIO_PINMUX_PAD3;
+	usart_conf.baudrate    = CONF_STDIO_BAUDRATE;
 
-static uint8_t write_buffer_2[2] = {
-	 0x01, 0xA0
-};
+	stdio_serial_init(&cdc_uart_module, CONF_STDIO_USART_MODULE,    
+&usart_conf);
+	usart_enable(&cdc_uart_module);
+}
 
-static uint8_t write_buffer_3[2] = {
-	 0x02, 0x01
-};
-
-static uint8_t read_buffer[7] = {
-		'a','a','a','a','a','a',0x0
-};
-//! [packet_data]
-
-//! [address]
-#define READ_ADDRESS 0x1E
-#define WRITE_ADDRESS 0x1E
-//! [address]
-
-/* Number of times to try to send packet if failed. */
-//! [timeout]
-#define TIMEOUT 1000
-//! [timeout]
-
-/* Init software module. */
-//! [dev_inst]
-struct i2c_master_module i2c_master_instance;
-//! [dev_inst]
-
-void configure_i2c_master(void);
-
-//! [initialize_i2c]
+/*
+	Configures I2C connection with standard settings
+*/
 void configure_i2c_master(void)
 {
 	/* Initialize config structure and software module. */
@@ -128,91 +136,12 @@ void configure_i2c_master(void)
 	i2c_master_enable(&i2c_master_instance);
 	//! [enable_module]
 }
-//! [initialize_i2c]
 
-int main(void)
-{
-	system_init();
-	
-	configure_console();
-	//console configuration
-
-	//! [init]
-	/* Configure device and enable. */
-	//! [config]
-	printf("Running...\n\r");
-	configure_i2c_master();
-	//! [config]
-
-	/* Timeout counter. */
-	//! [timeout_counter]
-
-	//! [timeout_counter]
-
-	/* Init i2c packet. */
-	//! [packet]
-	struct i2c_master_packet read_packet = {
-		.address     = READ_ADDRESS,
-		.data_length = 6,
-		.data        = read_buffer,
-		.ten_bit_address = false,
-		.high_speed      = false,
-		.hs_master_code  = 0x0,
-	};
-	
-	struct i2c_master_packet write_packet_1 = {
-		.address     = WRITE_ADDRESS,
-		.data_length = 2,
-		.data        = write_buffer_1,
-		.ten_bit_address = false,
-		.high_speed      = false,
-		.hs_master_code  = 0x0,
-	};
-	struct i2c_master_packet write_packet_2 = {
-		.address     = WRITE_ADDRESS,
-		.data_length = 2,
-		.data        = write_buffer_2,
-		.ten_bit_address = false,
-		.high_speed      = false,
-		.hs_master_code  = 0x0,
-	};
-	struct i2c_master_packet write_packet_3 = {
-		.address     = WRITE_ADDRESS,
-		.data_length = 2,
-		.data        = write_buffer_3,
-		.ten_bit_address = false,
-		.high_speed      = false,
-		.hs_master_code  = 0x0,
-	};
-	//! [packet]
-	//! [init]
-	//! [main]
-	/* Write buffer to slave until success. */
-	//! [write_packet]
-	//setup
-	write_command(&write_packet_1);
-	write_command(&write_packet_2);
-	
-	//measurement
-	while(true){
-	write_command(&write_packet_3);
-	read_command(&read_packet);
-	
-	//! [write_packet]
-
-	/* Read from slave until success. */
-	//! [read_packet]
-	printf("measurement\r\n");
-	printf("%x%x%x%x%x%x\r\n",(char)read_buffer[0],(char)read_buffer[0],(char)read_buffer[0],(char)read_buffer[0],(char)read_buffer[0],(char)read_buffer[0]);
-	//! [main]
-	}
-	while (true) {
-		/* Infinite loop */
-	}
-
-}
-
-void write_command(struct i2c_master_packet* packet_address){
+/*
+	Given a pointer to a packet, perform a write over I2C following the information
+	detailed in the packet
+*/
+void i2c_write_command(struct i2c_master_packet* packet_address){
 	uint16_t timeout = 0;
 	while (i2c_master_write_packet_wait(&i2c_master_instance, packet_address) !=
 	STATUS_OK) {
@@ -223,7 +152,11 @@ void write_command(struct i2c_master_packet* packet_address){
 	}
 }
 
-void read_command(struct i2c_master_packet* packet_address){
+/*
+	Given a pointer to a packet, perform a read over I2C following the information
+	detailed in the packet
+*/
+void i2c_read_command(struct i2c_master_packet* packet_address){
 	uint16_t timeout = 0;
 	while ((i2c_master_read_packet_wait(&i2c_master_instance, packet_address)) !=
 	STATUS_OK) {
@@ -232,4 +165,72 @@ void read_command(struct i2c_master_packet* packet_address){
 			printf("timeout");
 		}
 	}
+}
+
+/*
+	Perform a standard initialization of the HMC5883L Magnetometer
+	CURRENT SETTINGS:
+	15HZ default measurement rate
+	Gain = 5
+	Single Measurement Mode
+*/
+void HMC5883L_init(void){
+	static uint8_t write_buffer_1[2] = {
+		0x00, 0x70
+	};
+
+	static uint8_t write_buffer_2[2] = {
+		0x01, 0xA0
+	};
+	
+	struct i2c_master_packet write_packet_1 = {
+		.address     = HMC5883L_ADDRESS,
+		.data_length = 2,
+		.data        = write_buffer_1,
+		.ten_bit_address = false,
+		.high_speed      = false,
+		.hs_master_code  = 0x0,
+	};
+	struct i2c_master_packet write_packet_2 = {
+		.address     = HMC5883L_ADDRESS,
+		.data_length = 2,
+		.data        = write_buffer_2,
+		.ten_bit_address = false,
+		.high_speed      = false,
+		.hs_master_code  = 0x0,
+	};
+	
+	i2c_write_command(&write_packet_1);
+	i2c_write_command(&write_packet_2);
+}
+
+/*
+	Given an input buffer to read into (6 BYTES IN LENGTH MINIMUM) read from the x,y,z LSB and MSB registers
+	on the HMC5883L magnetometer in single measurement mode
+*/
+void HMC5883L_read(uint8_t* read_buffer){
+	struct i2c_master_packet read_packet = {
+		.address     = HMC5883L_ADDRESS,
+		.data_length = 6,
+		.data        = read_buffer,
+		.ten_bit_address = false,
+		.high_speed      = false,
+		.hs_master_code  = 0x0,
+	};
+	
+	static uint8_t write_buffer[2] = {
+		0x02, 0x01
+	};
+	
+	struct i2c_master_packet write_packet = {
+		.address     = HMC5883L_ADDRESS,
+		.data_length = 2,
+		.data        = write_buffer,
+		.ten_bit_address = false,
+		.high_speed      = false,
+		.hs_master_code  = 0x0,
+	};
+	
+	i2c_write_command(&write_packet);
+	i2c_read_command(&read_packet);
 }
