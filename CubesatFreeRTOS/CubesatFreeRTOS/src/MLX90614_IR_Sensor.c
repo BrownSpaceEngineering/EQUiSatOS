15a22b66b1b449401f98a44662b241bcfb2d373f
Arun Drelich <ad2476@gmail.com>
2015-09-30 22:22:20 -0400

    Sketch code for interfacing with MLX90614 IR Sensor over I2C

---
+++ CubesatFreeRTOS/CubesatFreeRTOS/src/MLX90614_IR_Sensor.c
@@ -0,0 +1,47 @@
+/*
+ * MLX9061_IR_Sensor.h
+ *
+ * Created: 30.09.2015 21:51
+ *  Author: Arun
+ */
+
+#include <MLX90614_IR_Sensor.h>
+
+/* Read data over I2C from IR Sensor
+ *
+ * buf: The 16-bit (2-byte) buffer pointer
+ * addr: The memory address to read from (address constants defined in header)
+ */
+
+/** NOTE: THIS IS NOT TESTED **/
+void MLX90614_read(uint8_t* buf, uint8_t addr) {
+  struct i2c_master_packet read_packet = {
+		.address     = MLX90614_READ_ADDR,
+		.data_length = 2,
+		.data        = buf,
+		.ten_bit_address = false,
+		.high_speed      = false,
+		.hs_master_code  = 0x0,
+	};
+	
+	static uint8_t write_buffer[2] = {
+		0x00, addr
+	};
+	
+	struct i2c_master_packet write_packet = {
+		.address     = MLX90614_WRITE_ADDR,
+		.data_length = 2,
+		.data        = write_buffer,
+		.ten_bit_address = false,
+		.high_speed      = false,
+		.hs_master_code  = 0x0,
+	};
+	
+	i2c_write_command(&write_packet);
+	i2c_read_command(&read_packet);
+}
+
+void MLX90614_write(uint8_t* buf) {
+
+}
+
---
+++ CubesatFreeRTOS/CubesatFreeRTOS/src/MLX90614_IR_Sensor.h
@@ -0,0 +1,23 @@
+/*
+ * MLX9061_IR_Sensor.h
+ *
+ * Created: 30.09.2015 21:47
+ *  Author: Arun
+ */
+
+#ifndef _MLX90614_IR_SENSOR_H_
+#define _MLX90614_IR_SENSOR_H_
+
+#include <I2C_Commands.h>
+
+#define MLX90614_WRITE_ADDR 0x5A
+#define MLX90614_READ_ADDR 0x5B
+
+#define TA_ADDR 0x006
+#define TOBJ1_ADDR 0x007
+#define TOBJ2_ADDR 0x008
+
+void MLX90614_read(uint8_t* buf);
+void MLX90614_write(uint8_t* buf);
+
+#endif
