c98add199640f0ca19864cbf0cdd278b0691cbd6
tylerfox <tyler_fox@brown.edu>
2015-10-04 14:36:37 -0400

    Radio test project

---
+++ radio/USART_QUICK_START1/USART_QUICK_START1/src/qs_usart_basic_use.c
@@ -0,0 +1,125 @@
+/**
+ * \file
+ *
+ * \brief SAM USART Quick Start
+ *
+ * Copyright (C) 2012-2015 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * 4. This software may only be redistributed and used in connection with an
+ *    Atmel microcontroller product.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+/*
+ * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
+ */
+#include <asf.h>
+#include "conf_uart_serial.h"
+#include "radio_commands.h"
+
+static struct usart_module cdc_uart_module;
+
+void configure_usart(void);
+
+Radio r;
+
+/**
+ *  Configure UART console.
+ */
+static void configure_console(void) {
+    struct usart_config usart_conf;
+
+    usart_get_config_defaults(&usart_conf);
+    usart_conf.mux_setting = CONF_STDIO_MUX_SETTING;
+    usart_conf.pinmux_pad0 = CONF_STDIO_PINMUX_PAD0;
+    usart_conf.pinmux_pad1 = CONF_STDIO_PINMUX_PAD1;
+    usart_conf.pinmux_pad2 = CONF_STDIO_PINMUX_PAD2;
+    usart_conf.pinmux_pad3 = CONF_STDIO_PINMUX_PAD3;
+    usart_conf.baudrate    = CONF_STDIO_BAUDRATE;
+
+    stdio_serial_init(&cdc_uart_module, CONF_STDIO_USART_MODULE,    
+&usart_conf);
+    usart_enable(&cdc_uart_module);
+}
+
+
+
+void configure_usart(void) {
+	struct usart_config config_usart;
+
+	usart_get_config_defaults(&config_usart);
+
+	config_usart.baudrate    = 9600;
+	config_usart.mux_setting = EDBG_CDC_SERCOM_MUX_SETTING;
+	config_usart.pinmux_pad0 = EDBG_CDC_SERCOM_PINMUX_PAD0;
+	config_usart.pinmux_pad1 = EDBG_CDC_SERCOM_PINMUX_PAD1;
+	config_usart.pinmux_pad2 = EDBG_CDC_SERCOM_PINMUX_PAD2;
+	config_usart.pinmux_pad3 = EDBG_CDC_SERCOM_PINMUX_PAD3;
+
+	/*
+	while (usart_init(&usart_instance,
+			EDBG_CDC_MODULE, &config_usart) != STATUS_OK) {
+	} */
+	
+	while (usart_init(r.in,
+		EDBG_CDC_MODULE, &config_usart) != STATUS_OK) {
+	}
+	
+	while (usart_init(r.out,
+		EDBG_CDC_MODULE, &config_usart) != STATUS_OK) {
+	}
+	usart_enable(r.in);
+	usart_enable(r.out);
+
+}
+
+int main(void)
+{
+	system_init();
+	configure_usart();
+	configure_console();	
+	program(&r);
+	
+//	uint8_t string[] = "Hello World!\r\n";
+	
+	/*
+	usart_write_buffer_wait(&usart_instance, string, sizeof(string));
+	uint16_t temp;
+	while (true) {
+		if (usart_read_wait(&usart_instance, &temp) == STATUS_OK) {
+
+			while (usart_write_wait(&usart_instance, temp) != STATUS_OK) {
+			}
+		}
+	}*/
+}
--- radio/USART_QUICK_START1/USART_QUICK_START1/src/radio_commands.c
+++ radio/USART_QUICK_START1/USART_QUICK_START1/src/radio_commands.c
@@ -6,27 +6,9 @@
 //  Copyright (c) 2015 Samuel Kortchmar. All rights reserved.
 //
 
-#include <stdio.h>
-#include <stdint.h>
 #include <asf.h>
-
-uint8_t computeCheckSum(uint8_t cmd[], int dataLength);
-int padCmd(uint8_t cmd[], int dataLength);
-void printByte(uint8_t byte);
-int simpleCommand(uint8_t controlChar, uint8_t dataByte, Radio* r);
-int simpleResponse(int responseType, int responseCode, Radio *r);
-int sendToRadio(uint8_t cmd[], int size, Radio* r);
-int receiveFromRadio(uint8_t* rx_data, uint16_t length, Radio* r);
-int setLinkSpeed(Radio* r);
-int setProtocol(Radio* r);
-int setModulationFormat(Radio* r);
-int setTxFrequencyByChannel(uint8_t channelNo, uint32_t freq, Radio* r);
-int setRxFrequencyByChannel(uint8_t channelNo, uint32_t freq, Radio* r);
-int program(Radio* r);
-int reset(uint8_t resetType, Radio* r);
-int setForwardErrorConnection(Radio* r);
-int setSquelch(uint8_t sensitivity, Radio* r);
-
+#include "radio_commands.h"
+#include "radio.h"
 //<><><<>><><>Helpers<><><<>><><>
 
 uint8_t computeCheckSum(uint8_t cmd[], int dataLength) {
