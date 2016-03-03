3f7d72aa2480a3a4fa70f46ad3bb8e471213d708
Arun Drelich <ad2476@gmail.com>
2016-03-02 21:01:15 -0500

    RENAMED some MLX functions. Added read EEPROM for SMBus addr

--- CubesatFreeRTOS/CubesatFreeRTOS/src/MLX90614_IR_Sensor.c
+++ CubesatFreeRTOS/CubesatFreeRTOS/src/MLX90614_IR_Sensor.c
@@ -1,18 +1,36 @@
 #include <MLX90614_IR_Sensor.h>
 
-void read_MLX90614(uint8_t* buf, uint8_t addr) {
+void MLX90614_setSMBusAddr(uint8_t device, uint8_t addr) {
+
+}
+
+uint16_t MLX90614_getSMBusAddr(uint8_t device) {
+  uint16_t data = 0xFFFF;
+
+  uint8_t read_buf[2] = {0x0, 0x0}; // MSB, LSB
+  MLX90614_read(device, MLX90614_EEPROM_SMBUS, read_buf);
+  data = *((uint16_t *)read_buf); // Derefence 2 bytes, MSB first
+
+  return data;
+}
+
+/* Read data from the MLX90614.
+    - SMBus_addr: The SMBus address of the MLX to read from
+    - ram_addr: memory address to read
+    - buf: destination buffer */
+void MLX90614_read(uint8_t SMBus_addr, uint8_t opcode, uint8_t* buf) {
 	
 	struct i2c_master_packet write_packet = {
-		.address     = MLX90614_I2CADDR,
+		.address     = SMBus_addr,
 		.data_length = 1,
-		.data        = &addr,
+		.data        = &opcode,
 		.ten_bit_address = false,
 		.high_speed      = false,
 		.hs_master_code  = 0x0,
 	};
 	
 	struct i2c_master_packet read_packet = {
-		.address     = MLX90614_I2CADDR,
+		.address     = SMBus_addr,
 		.data_length = 2,
 		.data        = buf,
 		.ten_bit_address = false,
@@ -24,23 +42,31 @@ void read_MLX90614(uint8_t* buf, uint8_t addr) {
 	i2c_read_command(&read_packet);
 }
 
-uint16_t MLX90614_readRawIRData(uint8_t addr) {
+/* Read the raw IR data from the MLX90614.
+    - SMBus_addr: The SMBus address of the MLX to read from
+    - channel: MLX90614 memory address to read (i.e. raw IR channel 1 or 2)
+   Returns: Raw IR data for given channel address */
+uint16_t MLX90614_readRawIRData(uint8_t SMBus_addr, uint8_t channel) {
 	uint16_t data = 0xFFFF;
 
-	if (addr == MLX90614_RAWIR1 || addr == MLX90614_RAWIR2) {
+	if (channel == MLX90614_RAWIR1 || channel == MLX90614_RAWIR2) {
 		uint8_t read_buf[2] = {0x0, 0x0}; // MSB, LSB
-		read_MLX90614(read_buf, MLX90614_RAWIR1);
+		MLX90614_read(SMBus_addr, channel, read_buf);
 		data = *((uint16_t *)read_buf); // Cast pointer to dereference 2 bytes, MSB first
 	}
 
 	return data;
 }
 
-float readTemp(uint8_t addr) {
+/* Read the temp data (TODO: what exactly does this do?)
+    - SMBus_addr: The SMBus address of the MLX to read from
+    - ram_addr: memory address to read
+   Return temperature in degrees C (?) */
+float MLX90614_readTemp(uint8_t SMBus_addr, uint8_t ram_addr) {
 	uint8_t read_buffer[2] = {
 		0x0,0x0
 	};
-	read_MLX90614(read_buffer,addr);
+	MLX90614_read(SMBus_addr, ram_addr, read_buffer);
 	uint16_t res = read_buffer[0] | (((uint16_t)read_buffer[1]) << 8);
 	float temp = (float) res;
 	temp *= .02;
@@ -48,11 +74,11 @@ float readTemp(uint8_t addr) {
 	return temp;
 }
 
-float MLX90614_readObjectTempC() {
-	return readTemp(MLX90614_TOBJ1);
+float MLX90614_readObjectTempC(uint8_t SMBus_addr) {
+	return MLX90614_readTemp(SMBus_addr, MLX90614_TOBJ1);
 }
 
 
-float MLX90614_readAmbientTempC() {
-	return readTemp(MLX90614_TA);
+float MLX90614_readAmbientTempC(uint8_t SMBus_addr) {
+	return MLX90614_readTemp(SMBus_addr, MLX90614_TA);
 }
--- CubesatFreeRTOS/CubesatFreeRTOS/src/MLX90614_IR_Sensor.h
+++ CubesatFreeRTOS/CubesatFreeRTOS/src/MLX90614_IR_Sensor.h
@@ -3,20 +3,31 @@
 
 #include <I2C_Commands.h>
 
+/* Opcode bitflags: */
+#define MLX90614_EEPROM_OP 0x20
+#define MLX90614_RAM_OP 0x00
+
+/* MLX90614 address(es): */
 #define MLX90614_I2CADDR 0x5A
 
-#define MLX90614_RAWIR1 0x04
-#define MLX90614_RAWIR2 0x05
-#define MLX90614_TA 0x06
-#define MLX90614_TOBJ1 0x07
-#define MLX90614_TOBJ2 0x08
+/* MLX90614 EEPROM addresses: */
+#define MLX90614_EEPROM_SMBUS (0x0E | MLX90614_EEPROM_OP)
 
-void read_MLX90614(uint8_t* buf, uint8_t addr);
-uint16_t MLX90614_readRawIRData(uint8_t addr);
-float readTemp(uint8_t addr);
-float MLX90614_readObjectTempC();
-float MLX90614_readAmbientTempC();
+/* MLX90614 RAM addresses: */
+#define MLX90614_RAWIR1 (0x04 | MLX90614_RAM_OP)
+#define MLX90614_RAWIR2 (0x05 | MLX90614_RAM_OP)
+#define MLX90614_TA (0x06 | MLX90614_RAM_OP)
+#define MLX90614_TOBJ1 (0x07 | MLX90614_RAM_OP)
+#define MLX90614_TOBJ2 (0x08 | MLX90614_RAM_OP)
 
+void MLX90614_read(uint8_t SMBus_addr, uint8_t ram_addr, uint8_t* buf);
+uint16_t MLX90614_readRawIRData(uint8_t SMBus_addr, uint8_t channel);
+float MLX90614_readTemp(uint8_t SMBus_addr, uint8_t ram_addr);
+float MLX90614_readObjectTempC(uint8_t SMBus_addr);
+float MLX90614_readAmbientTempC(uint8_t SMBus_addr);
+
+/* UNUSED. Could be used to poll both raw IR channels without
+   specifiying. */
 struct mlxRawData {uint16_t IR_chan1; uint16_t IR_chan2;};
 
 #endif
