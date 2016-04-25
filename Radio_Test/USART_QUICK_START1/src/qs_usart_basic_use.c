0e78f57b2324555e626226e75ed8ef9f61d3bb6d
tylerfox <tyler_fox@brown.edu>
2016-04-25 00:41:18 -0400

    made checksum unsigned

--- Radio_Test/USART_QUICK_START1/src/qs_usart_basic_use.c
+++ Radio_Test/USART_QUICK_START1/src/qs_usart_basic_use.c
@@ -225,7 +225,9 @@ void usart_send_string(const char *str_buf)
 		SERCOM2->USART.DATA.reg = *str_buf;
 		str_buf++;
 	}
-}void print(const char *str_buf)
+}
+
+void print(const char *str_buf)
 {
 	while (*str_buf != '\0')
 	{
@@ -233,26 +235,151 @@ void usart_send_string(const char *str_buf)
 		SERCOM3->USART.DATA.reg = *str_buf;
 		str_buf++;
 	}
-}void setCommandMode() {	
+}
+
+void setCommandMode() {	
 	usart_send_string("+++");
-	delay_ms(300);}void setDealerMode() {		sendbuffer[0] = 0x01;	sendbuffer[1] = 0x44;
+	delay_ms(300);
+}
+
+void setDealerMode() {	
+	sendbuffer[0] = 0x01;
+	sendbuffer[1] = 0x44;
 	sendbuffer[2] = 0x01;
 	sendbuffer[3] = ~0x45;
 	sendbuffer[4] = '\0';		
-	usart_send_string(sendbuffer);}void setTxFreq() {	//index 3-6 is 4 byte frequency in Hz	sendbuffer[0] = 0x01;	sendbuffer[1] = 0x37;	sendbuffer[2] = 0x01; //channel 1	sendbuffer[3] = 0x1A;	sendbuffer[4] = 0x0C;	sendbuffer[5] = 0x17;	sendbuffer[6] = 0x40;	sendbuffer[7] = ~0xB5;	sendbuffer[8] = '\0';
-	usart_send_string(sendbuffer);}void setRxFreq() {	//index 3-6 is 4 byte frequency in Hz	sendbuffer[0] = 0x01;	sendbuffer[1] = 0x39;	sendbuffer[2] = 0x01; //channel 1	sendbuffer[3] = 0x1A;	sendbuffer[4] = 0x0C;	sendbuffer[5] = 0x17;	sendbuffer[6] = 0x40;	sendbuffer[7] = ~0xB7;	sendbuffer[8] = '\0';
-	usart_send_string(sendbuffer);}void setChannel() {	//index 2 is byte to set channel
+	usart_send_string(sendbuffer);
+}
+
+void setTxFreq() {
+	//index 3-6 is 4 byte frequency in Hz
+	sendbuffer[0] = 0x01;
+	sendbuffer[1] = 0x37;
+	sendbuffer[2] = 0x01; //channel 1
+	sendbuffer[3] = 0x1A;
+	sendbuffer[4] = 0x0C;
+	sendbuffer[5] = 0x17;
+	sendbuffer[6] = 0x40;
+	sendbuffer[7] = ~0xB5;
+	sendbuffer[8] = '\0';
+	usart_send_string(sendbuffer);
+}
+
+void setRxFreq() {
+	//index 3-6 is 4 byte frequency in Hz
+	sendbuffer[0] = 0x01;
+	sendbuffer[1] = 0x39;
+	sendbuffer[2] = 0x01; //channel 1
+	sendbuffer[3] = 0x1A;
+	sendbuffer[4] = 0x0C;
+	sendbuffer[5] = 0x17;
+	sendbuffer[6] = 0x40;
+	sendbuffer[7] = ~0xB7;
+	sendbuffer[8] = '\0';
+	usart_send_string(sendbuffer);
+}
+
+void setChannel() {
+	//index 2 is byte to set channel
 	sendbuffer[0] = 0x01;
 	sendbuffer[1] = 0x03;
 	sendbuffer[2] = 0x01;
 	sendbuffer[3] = 0xFB;
 	sendbuffer[4] = '\0';	
-	usart_send_string(sendbuffer);}void warmReset(){	sendbuffer[0] = 0x01;
+	usart_send_string(sendbuffer);
+}
+
+void warmReset(){
+	sendbuffer[0] = 0x01;
 	sendbuffer[1] = 0x1d;
 	sendbuffer[2] = 0x01; //warm
 	sendbuffer[3] = ~0x1E;
 	sendbuffer[4] = '\0';
-	usart_send_string(sendbuffer);}void setModulationFormat(){	sendbuffer[0] = 0x01;	sendbuffer[1] = 0x2B;	sendbuffer[2] = 0x01;	sendbuffer[3] = ~0x2C;	sendbuffer[4] = '\0';	usart_send_string(sendbuffer);}void setLinkSpeed(){	sendbuffer[0] = 0x01;	sendbuffer[1] = 0x05;	sendbuffer[2] = 0x03;	sendbuffer[3] = ~0x08;	sendbuffer[4] = '\0';	usart_send_string(sendbuffer);}char calculateChecksum(char* data, int dataLen){	char checksum = 0;	for(int i=0;i<dataLen;i++) checksum = (checksum+data[i])&0xFF;	return ~checksum;}int responseCheck(char arr[]){	if(working==0) return 0; //not gonna work anymore pal	for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){		if(receivebuffer[i]!=arr[i]) return 0;	}	return 1;}char dealer_response[4] = {1, 196, 0, 59};char txFreq_response[4] = {1, 183, 0, 72};char rxFreq_response[4] = {1, 185, 0, 70};char channel_response[4] = {0x01, 0x83, 0x00, 0x7c};char warmReset_response[4] = {0x01, 0x9d, 0x00, 0x62};	void initializeRadio() {	setCommandMode();	delay_ms(100); //remember to put delay, because the MCU is faster than the USART		setDealerMode();	delay_ms(100);	working = responseCheck(dealer_response);	print(receivebuffer);			setTxFreq();	delay_ms(100);	working = responseCheck(txFreq_response);	print(receivebuffer);		setRxFreq();	delay_ms(100);	working = responseCheck(rxFreq_response);	print(receivebuffer);			setModulationFormat();	delay_ms(400);	print(receivebuffer);		setLinkSpeed();	delay_ms(100);	print(receivebuffer);		setChannel();		delay_ms(500); //longer delay because radio is from the 90s and needs a smoke break	working = responseCheck(channel_response);	print(receivebuffer);		warmReset();	delay_ms(500); //longer delay because radio is from the 90s and needs a smoke break	working = responseCheck(warmReset_response);	print(receivebuffer);	}
+	usart_send_string(sendbuffer);
+}
+
+void setModulationFormat(){
+	sendbuffer[0] = 0x01;
+	sendbuffer[1] = 0x2B;
+	sendbuffer[2] = 0x01;
+	sendbuffer[3] = ~0x2C;
+	sendbuffer[4] = '\0';
+	usart_send_string(sendbuffer);
+}
+
+void setLinkSpeed(){
+	sendbuffer[0] = 0x01;
+	sendbuffer[1] = 0x05;
+	sendbuffer[2] = 0x03;
+	sendbuffer[3] = ~0x08;
+	sendbuffer[4] = '\0';
+	usart_send_string(sendbuffer);
+}
+
+unsigned char calculateChecksum(char* data, int dataLen) {
+    unsigned char checksum = 0;
+    for (int i = 0; i<dataLen; i++) checksum = (checksum + data[i]) & 0xFF;
+    return ~checksum;
+}
+
+int responseCheck(char arr[]){
+	if(working==0) return 0; //not gonna work anymore pal
+
+	for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
+		if(receivebuffer[i]!=arr[i]) return 0;
+	}
+	return 1;
+}
+
+
+char dealer_response[4] = {1, 196, 0, 59};
+char txFreq_response[4] = {1, 183, 0, 72};
+char rxFreq_response[4] = {1, 185, 0, 70};
+char channel_response[4] = {0x01, 0x83, 0x00, 0x7c};
+char warmReset_response[4] = {0x01, 0x9d, 0x00, 0x62};
+	
+void initializeRadio() {
+	setCommandMode();
+	delay_ms(100); //remember to put delay, because the MCU is faster than the USART
+	
+	setDealerMode();
+	delay_ms(100);
+	working = responseCheck(dealer_response);
+	print(receivebuffer);
+	
+	
+	setTxFreq();
+	delay_ms(100);
+	working = responseCheck(txFreq_response);
+	print(receivebuffer);
+	
+	setRxFreq();
+	delay_ms(100);
+	working = responseCheck(rxFreq_response);
+	print(receivebuffer);	
+	
+	setModulationFormat();
+	delay_ms(400);
+	print(receivebuffer);
+	
+	setLinkSpeed();
+	delay_ms(100);
+	print(receivebuffer);
+	
+	setChannel();	
+	delay_ms(500); //longer delay because radio is from the 90s and needs a smoke break
+	working = responseCheck(channel_response);
+	print(receivebuffer);
+	
+	warmReset();
+	delay_ms(500); //longer delay because radio is from the 90s and needs a smoke break
+	working = responseCheck(warmReset_response);
+	print(receivebuffer);
+	
+}
+
+
+
 int main (void)
 {
 system_init();
@@ -323,4 +450,4 @@ port_pin_set_output_level(PIN_PA10, false);*/
 	print(receivebuffer);*/
 		
 //}
-}
+}
