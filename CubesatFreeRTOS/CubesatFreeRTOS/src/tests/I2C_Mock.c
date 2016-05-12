08ddb9b0c0ba4f9e11b4038098f2c8adb3219439
unknown <zachary_nado@brown.edu>
2016-05-12 04:37:36 -0400

    removing some getters to make all I2C mock variable access consistent, using direct access, may want to change to all getters.  also added device read/write address checking

--- CubesatFreeRTOS/CubesatFreeRTOS/src/tests/I2C_Mock.c
+++ CubesatFreeRTOS/CubesatFreeRTOS/src/tests/I2C_Mock.c
@@ -1,33 +1,25 @@
 #include "I2C_Mock.h"
 
-int is_configured = 0;
-
+// define here
 int no_stop_calls = 0;
-
-const struct test_case *test = NULL;
-
-uint8_t* next_read_bytes = NULL;
-
-uint8_t* written_bytes = NULL;
+const struct test_case *i2c_mock_test = NULL;
+uint8_t* i2c_mock_next_read_bytes = NULL;
+uint8_t* i2c_mock_written_bytes = NULL;
 int written_bytes_len = 0;
+uint8_t i2c_mock_read_addr;
+uint8_t i2c_mock_write_addr;
 
 void set_i2c_mock_test_case(const struct test_case *_test) {
-	test = _test;
+	i2c_mock_test = _test;
 }
 
 void cleanup_i2c_mock_test_case(void) {
-	free(written_bytes);
+	free(i2c_mock_written_bytes);
 	written_bytes_len = 0;
 	no_stop_calls = 0;
 }
 
-void configure_i2c_master_mock(void) {
-	is_configured = 1;
-}
-
-void i2c_write_mock(struct i2c_master_packet* packet_address) {
-	test_assert_true(test, is_configured, "I2C not configured.");
-	
+void i2c_write_mock(struct i2c_master_packet* packet_address) {	
 	int old_written_bytes_len = written_bytes_len;
 	written_bytes_len += packet_address->data_length;
 	
@@ -35,34 +27,28 @@ void i2c_write_mock(struct i2c_master_packet* packet_address) {
 	
 	// set each element of the new buffer
 	for(int i=0; i<old_written_bytes_len; i++) {
-		new_written_bytes[i] = written_bytes[i];
+		new_written_bytes[i] = i2c_mock_written_bytes[i];
 	}
 	for(int i=old_written_bytes_len; i<written_bytes_len; i++) {
 		new_written_bytes[i] = packet_address->data[i];
 	}
-
-
-	free(written_bytes);
-	written_bytes = new_written_bytes;
+	free(i2c_mock_written_bytes);
+	i2c_mock_written_bytes = new_written_bytes;
+	
+	i2c_mock_write_addr = packet_address->address;
 }
 
-uint8_t* get_written_bytes(void) {
-	return written_bytes;
-}
 
 void i2c_write_mock_no_stop(struct i2c_master_packet* packet_address) {
-	test_assert_true(test, is_configured, "I2C not configured.");
+	no_stop_calls++;
 	i2c_write_mock(packet_address);
 }
 
 void i2c_read_mock(struct i2c_master_packet* packet_address) {
-	packet_address->data = next_read_bytes;
+	packet_address->data = i2c_mock_next_read_bytes;
+	i2c_mock_read_addr = packet_address->address;
 }
 
 void i2c_mock_set_next_read_bytes(uint8_t* _next_read_bytes) {
-	next_read_bytes = _next_read_bytes;
-}
-
-int get_no_stop_call_count(void) {
-	return no_stop_calls;
+	i2c_mock_next_read_bytes = _next_read_bytes;
 }
--- CubesatFreeRTOS/CubesatFreeRTOS/src/tests/I2C_Mock.h
+++ CubesatFreeRTOS/CubesatFreeRTOS/src/tests/I2C_Mock.h
@@ -1,27 +1,24 @@
-/*
- * IncFile1.h
- *
- * Created: 4/26/2015 3:34:57 PM
- *  Author: Daniel
- */ 
-
-
 #ifndef I2C_MOCK_H
 #define I2C_MOCK_H
 
 #include <asf.h>
-// #include <I2C_Commands.h>
+
+// declare here (may want to use getters instead?)
+extern int no_stop_calls;
+extern const struct test_case *i2c_mock_test;
+extern uint8_t* i2c_mock_next_read_bytes;
+extern uint8_t* i2c_mock_written_bytes;
+extern int written_bytes_len;
+extern uint8_t i2c_mock_read_addr;
+extern uint8_t i2c_mock_write_addr;
 
 void set_i2c_mock_test_case(const struct test_case *_test);
 void cleanup_i2c_mock_test_case(void);
-
-uint8_t* get_written_bytes(void);
 
 void configure_i2c_master_mock(void);
 void i2c_write_mock(struct i2c_master_packet* packet_address);
 void i2c_write_mock_no_stop(struct i2c_master_packet* packet_address);
 void i2c_read_mock(struct i2c_master_packet* packet_address);
 void i2c_mock_set_next_read_bytes(uint8_t* _next_read_bytes);
-int get_no_stop_call_count(void);
 
 #endif /* I2C_MOCK_H */
