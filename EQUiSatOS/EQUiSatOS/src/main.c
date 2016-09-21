5f79affb69d2b430ca0d16ec7ffcec13aa26c7b1
Arun Drelich <ad2476@gmail.com>
2016-09-20 20:41:16 -0400

    Focusing branch on Gustavo's MRAM bootloading work

--- EQUiSatOS/EQUiSatOS/src/main.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-	runit();
-}
--- EQUiSatOS/EQUiSatOS/src/testing_functions/bit_flipping_tester.h
+++
@@ -1,9 +0,0 @@
-/*
- * bit_flipping_tester.h: See bit_flipping_tester.c for detailed
- *  description of this unit test.
- *
- * Author: Arun Drelich
- */
-
-void init_bitFlip_test(void); // "Public method" for the unit test
-
