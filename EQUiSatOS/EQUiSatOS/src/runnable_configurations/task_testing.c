7956267158ed14838ff131e4a99860f41c339d18
MckennaCisler <mckennacisler@gmail.com>
2017-02-15 20:58:25 -0500

    Post-flow meeting changes

---
+++ EQUiSatOS/EQUiSatOS/src/runnable_configurations/task_testing.c
@@ -0,0 +1,35 @@
+/*
+ * task_testing.c
+ *
+ * Created: 1/30/2017 3:56:53 PM
+ *  Author: mckenna
+ */
+
+#include "task_testing.h"
+
+int i = 0;
+
+void task_suicide_test(void *pvParameters) 
+{
+	// initialize xNextWakeTime once
+	TickType_t xNextWakeTime = xTaskGetTickCount();
+
+	for( ;; )
+	{
+		// block for a time 
+		vTaskDelayUntil( &xNextWakeTime, 1000 / portTICK_PERIOD_MS);
+		
+		while (i <= 100000) {
+			if (i >= 50000) {
+				vTaskSuspend(suicide_test_handle);
+				int b = 0;
+			}	
+			i++;
+		}
+		
+		int b = 0;
+		
+	}
+	// delete this task if it ever breaks out
+	vTaskDelete( NULL );	
+}
