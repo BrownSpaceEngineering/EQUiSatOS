7e8dbe0fc0e523b0ee582d6c88e573b37d7c2085
MckennaCisler <mckennacisler@gmail.com>
2017-03-22 22:32:42 -0400

    Finish constant testing

--- EQUiSatOS/EQUiSatOS/src/runnable_configurations/init_rtos_tasks.h
+++ EQUiSatOS/EQUiSatOS/src/runnable_configurations/init_rtos_tasks.h
@@ -13,13 +13,8 @@
 #include "struct_tests.h"
 #include "stacks/equistack.h"
 
-// /* Global task frequencies to allow for changing them in different states */
-// We shouldn't need to change more than a few of these... it might be better to just use variables
-// int taskFrequencies[6]; //NUM_TASKS - DONT KNOW WHY THIS DOESNT WORK
-
 /* Global satellite state - is this the best way to do this? */
 int8_t CurrentState;
-int8_t CurrentLoggingState;
 
 void runit_2(void);
 
@@ -36,7 +31,6 @@ TaskHandle_t flash_data_task_handle;
 TaskHandle_t transmit_data_task_handle;
 TaskHandle_t attitude_data_task_handle;
 
-// TODO: Do we need to change states?
 void set_state_hello_world(void);
 void set_state_idle(void);
 void set_state_low_power(void);
