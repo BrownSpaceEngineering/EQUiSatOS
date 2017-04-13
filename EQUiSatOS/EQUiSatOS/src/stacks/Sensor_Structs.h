41baf7d09593025b1c7293477c977e5ec22effca
MckennaCisler <mckennacisler@gmail.com>
2017-04-12 20:41:44 -0400

    Change structs to simply be aliases for internal values
    This was a hard decision, but we made it

--- EQUiSatOS/EQUiSatOS/src/stacks/Sensor_Structs.h
+++ EQUiSatOS/EQUiSatOS/src/stacks/Sensor_Structs.h
@@ -10,87 +10,21 @@
 
 #include <asf.h>
 
-// TODO: Do we want a state struct?
-// TODO: Parity bits?
-// TODO: Make sure these are the batches we want
-// TODO: Does it make sense wrap these arrays in structs?????
-
-typedef struct lion_volts_batch
-{
-	uint16_t values[2];
-} lion_volts_batch;
-
-typedef struct lion_current_batch
-{
-	uint16_t values[2];
-} lion_current_batch;
-
-typedef struct led_temps_batch
-{
-	uint16_t values[4];
-} led_temps_batch;
-
-typedef struct lifepo_current_batch
-{
-	uint16_t values[4];
-} lifepo_current_batch;
-
-typedef struct lifepo_volts_batch
-{
-	uint16_t values[4];
-} lifepo_volts_batch;
-
-typedef struct bat_temp_batch
-{
-	uint16_t values[4];
-} bat_temp_batch;
-
-typedef struct ir_temps_batch
-{
-	uint16_t values[6];
-} ir_temps_batch;
-
-typedef struct radio_volts_batch
-{
-	uint16_t values[2];
-} radio_volts_batch;
-
-typedef struct bat_charge_volts_batch
-{
-	uint16_t values[14];
-} bat_charge_volts_batch;
-
-typedef struct bat_charge_dig_sigs_batch
-{
-	uint16_t value;
-} bat_charge_dig_sigs_batch;
-
-typedef struct digital_out_batch
-{
-	uint16_t value;
-} digital_out_batch;
-
-typedef struct ir_batch
-{
-	uint16_t values[12];
-} ir_batch;
-
-typedef struct diode_batch
-{
-	uint8_t values[6];
-} diode_batch;
-
-typedef struct led_current_batch
-{
-	uint16_t values[4];
-} led_current_batch;
-
-typedef struct magnetometer_batch
-{
-	// one for each axis
-	uint16_t values[3];
-} magnetometer_batch;
-
+typedef uint16_t* lion_volts_batch;				// array of size 2
+typedef uint16_t* lion_current_batch;			// array of size 2
+typedef uint16_t* led_temps_batch;				// array of size 4
+typedef uint16_t* lifepo_current_batch;			// array of size 4
+typedef uint16_t* lifepo_volts_batch;			// array of size 4
+typedef uint16_t* bat_temp_batch;				// array of size 4
+typedef uint16_t* ir_temps_batch;				// array of size 6
+typedef uint16_t* radio_volts_batch;			// array of size 2
+typedef uint16_t* bat_charge_volts_batch;		// array of size 14
+typedef uint16_t bat_charge_dig_sigs_batch;		// single value
+typedef uint16_t digital_out_batch;				// single value
+typedef uint16_t* ir_batch;						// array of size 12
+typedef uint8_t* diode_batch;					// array of size 6
+typedef uint16_t* led_current_batch;			// array of size 4
+typedef uint16_t* magnetometer_batch;			// array of size 3 (one for each axis)
 typedef struct imu_batch
 {
 	// one for each axis, for each sensor
@@ -98,10 +32,6 @@ typedef struct imu_batch
 	uint16_t gyro[3];
 	uint16_t magnetometer[3];
 } imu_batch;
-
-typedef struct radio_temp_batch
-{
-	uint8_t value;
-} radio_temp_batch;
+typedef uint8_t radio_temp_batch;				// single value
 
 #endif
