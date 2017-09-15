fad89eb58f7922ec3be336d031730e957a1f8d15
MckennaCisler <mckennacisler@gmail.com>
2017-09-15 00:50:07 -0400

    Finish main rebase; now working on making it build
    Re-added libraries and added back files that got excluded in .cproj
    
    Also, TODO: .gitignore .atsuo, Makefile, and makedep.mk for future rebases!

--- EQUiSatOS/EQUiSatOS/Debug/makedep.mk
+++ EQUiSatOS/EQUiSatOS/Debug/makedep.mk
@@ -1,140 +1,20 @@
-<<<<<<< 067898aafe6e08d07ce6404c2afcdb435391a4ed
 ################################################################################
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-src\ASF\sam0\drivers\adc\adc_sam_d_r\adc.c
+src\processor_drivers\PWM_Commands.c
 
-src\ASF\sam0\drivers\adc\adc_sam_d_r\adc_callback.c
+src\processor_drivers\Watchdog_Commands.c
 
-src\ASF\sam0\drivers\sercom\i2c\i2c_sam0\i2c_slave.c
+src\processor_drivers\Watchdog_Task.c
 
-src\ASF\sam0\drivers\sercom\i2c\i2c_sam0\i2c_slave_interrupt.c
+src\runnable_configurations\flatsat.c
 
-src\ASF\sam0\drivers\sercom\sercom_interrupt.c
+src\runnable_configurations\isItOn.c
 
-src\ASF\sam0\drivers\tcc\tcc.c
-
-src\ASF\sam0\drivers\tcc\tcc_callback.c
-
-src\ASF\thirdparty\freertos\freertos-8.0.1\Source\portable\MemMang\heap_1.c
-
-src\runnable_configurations\rtos_task_frequencies.c
-
-src\runnable_configurations\struct_tests.c
-
-src\runnable_configurations\task_testing.c
-
-src\stacks\equistack.c
-
-src\stacks\package_transmission.c
-
-src\stacks\State_Structs.c
-
-src\stacks\test_data.c
-
-src\testing_functions\test_data.c
-
-src\testing_functions\test_stacks.c
-
-src\testing_functions\test_structs.c
-
-src\ASF\common\utils\interrupt\interrupt_sam_nvic.c
-
-src\ASF\sam0\boards\samd21_xplained_pro\board_init.c
+src\testing_functions\watchdog_task_test.c
 
 src\ASF\sam0\drivers\adc\adc_sam_d_r\adc.c
-
-src\ASF\sam0\drivers\rtc\rtc_sam_d_r\rtc_calendar.c
-
-src\ASF\sam0\drivers\rtc\rtc_sam_d_r\rtc_calendar_interrupt.c
-
-src\ASF\sam0\drivers\sercom\i2c\i2c_sam0\i2c_master.c
-
-src\ASF\sam0\drivers\system\clock\clock_samd21_r21_da\clock.c
-
-src\ASF\sam0\drivers\system\clock\clock_samd21_r21_da\gclk.c
-
-src\ASF\sam0\drivers\system\interrupt\system_interrupt.c
-
-src\ASF\sam0\drivers\system\pinmux\pinmux.c
-
-src\ASF\sam0\drivers\system\system.c
-
-src\ASF\sam0\utils\cmsis\samd21\source\gcc\startup_samd21.c
-
-src\ASF\sam0\utils\cmsis\samd21\source\system_samd21.c
-
-src\ASF\sam0\utils\syscalls\gcc\syscalls.c
-
-src\main.c
-
-src\ASF\sam0\drivers\sercom\spi\spi.c
-
-src\ASF\sam0\utils\stdio\read.c
-
-src\ASF\sam0\utils\stdio\write.c
-
-src\ASF\thirdparty\freertos\freertos-8.0.1\Source\croutine.c
-
-src\ASF\thirdparty\freertos\freertos-8.0.1\Source\event_groups.c
-
-src\ASF\thirdparty\freertos\freertos-8.0.1\Source\portable\GCC\ARM_CM0\port.c
-
-src\ASF\thirdparty\freertos\freertos-8.0.1\Source\list.c
-
-src\ASF\thirdparty\freertos\freertos-8.0.1\Source\queue.c
-
-src\ASF\thirdparty\freertos\freertos-8.0.1\Source\tasks.c
-
-src\ASF\thirdparty\freertos\freertos-8.0.1\Source\timers.c
-
-src\ASF\sam0\drivers\sercom\usart\usart.c
-
-src\ASF\sam0\drivers\sercom\sercom.c
-
-src\ASF\common2\services\delay\sam0\systick_counter.c
-
-src\ASF\common\utils\unit_test\suite.c
-
-src\ASF\sam0\drivers\nvm\nvm.c
-
-src\ASF\sam0\drivers\port\port.c
-
-src\processor_drivers\ADC_Commands.c
-
-src\processor_drivers\Direct_Pin_Commands.c
-
-src\processor_drivers\I2C_Commands.c
-
-src\processor_drivers\RTC_Commands.c
-
-src\processor_drivers\SPI_Commands.c
-
-src\processor_drivers\USART_Commands.c
-
-src\radio\Radio_Commands.c
-
-src\runnable_configurations\init_rtos_tasks.c
-
-src\runnable_configurations\rtos_tasks.c
-
-src\runnable_configurations\scratch_testing.c
-
-src\sensor_drivers\HMC5883L_Magnetometer_Commands.c
-
-src\sensor_drivers\MLX90614_IR_Sensor.c
-
-src\sensor_drivers\sensor_read_commands.c
-
-src\sensor_drivers\switching_commands.c
-
-src\sensor_drivers\TEMD6200_Commands.c
-
-=======
-################################################################################
-# Automatically-generated file. Do not edit or delete the file
-################################################################################
 
 src\ASF\sam0\drivers\adc\adc_sam_d_r\adc_callback.c
 
@@ -158,9 +38,19 @@ src\runnable_configurations\struct_tests.c
 
 src\runnable_configurations\task_testing.c
 
+src\sensor_drivers\AD7991_ADC.c
+
+src\sensor_drivers\LTC1380_Multiplexer_Commands.c
+
+src\sensor_drivers\MPU9250_9axis_Commands.c
+
+src\sensor_drivers\TCA9535_GPIO.c
+
 src\stacks\equistack.c
 
 src\stacks\package_transmission.c
+
+src\stacks\State_Structs.c
 
 src\testing_functions\test_data.c
 
@@ -171,8 +61,6 @@ src\testing_functions\test_structs.c
 src\ASF\common\utils\interrupt\interrupt_sam_nvic.c
 
 src\ASF\sam0\boards\samd21_xplained_pro\board_init.c
-
-src\ASF\sam0\drivers\adc\adc_sam_d_r\adc.c
 
 src\ASF\sam0\drivers\rtc\rtc_sam_d_r\rtc_calendar.c
 
@@ -236,8 +124,6 @@ src\runnable_configurations\rtos_tasks.c
 
 src\runnable_configurations\scratch_testing.c
 
-src\sensor_drivers\HMC5883L_Magnetometer_Commands.c
-
 src\sensor_drivers\MLX90614_IR_Sensor.c
 
 src\sensor_drivers\sensor_read_commands.c
@@ -246,4 +132,3 @@ src\sensor_drivers\switching_commands.c
 
 src\sensor_drivers\TEMD6200_Commands.c
 
->>>>>>> Switch to RTOS 9
--- EQUiSatOS/EQUiSatOS/src/asf.h
+++ EQUiSatOS/EQUiSatOS/src/asf.h
@@ -52,8 +52,9 @@
  * running the ASF driver selector tool. Any changes will be discarded.
  */
 
-// From module: ADC - Analog-to-Digital Converter (Polled APIs)
+// From module: ADC - Analog-to-Digital Converter (Callback APIs)
 #include <adc.h>
+#include <adc_callback.h>
 
 // From module: Common SAM0 compiler driver
 #include <compiler.h>
@@ -134,17 +135,10 @@
 // From module: Standard serial I/O (stdio)
 #include <stdio_serial.h>
 
-// From module: TCC - Timer Counter for Control Applications (Polled APIs)
-#include <tcc.h>
-
 // From module: USART - Serial interface- SAM implementation for devices with only USART
 #include <serial.h>
 
 // From module: Unit test framework - SAM0 implementation
 #include <unit_test/suite.h>
-
-// From module: WDT - Watchdog Timer (Callback APIs)
-#include <wdt.h>
-#include <wdt_callback.h>
 
 #endif // ASF_H
