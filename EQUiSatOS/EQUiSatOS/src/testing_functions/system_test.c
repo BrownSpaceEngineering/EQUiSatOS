c6699f315f34f60b1110082aefc47f709c62b7ec
JarodBoone <jtboone12@gmail.com>
2017-10-22 18:11:15 -0400

    added control board test for remote ADC

--- EQUiSatOS/EQUiSatOS/src/testing_functions/system_test.c
+++ EQUiSatOS/EQUiSatOS/src/testing_functions/system_test.c
@@ -47,7 +47,7 @@ float readVoltagemV(struct adc_module adc_instance) {
 
 float MLX90614_test(uint8_t addr){
 	 
-	 configure_i2c_standard(SERCOM4); //SERCOM4 -> I2C serial port
+	 //configure_i2c_standard(SERCOM4); //SERCOM4 -> I2C serial port
 	 
 	 //DON'T NEED TO TURN ON FOR TEST BOARD. UNCOMMENT WHEN REAL TESTING
 	 MLX90614_init(); //Turns on the IR sensor and sets up the Port
@@ -124,12 +124,136 @@ float TEMD6200_test(){
 
 }
 
+//input a uint8_t array of length 4, it will fill each index with the corresponding test
+// channel 0 -> results[0],  channel 1 -> results[1] etc...
+// 1 represents a failed channel test 0 represents a passed channel test
+void AD7991_control_test(uint8_t *results){
+	
+	const uint16_t error_tolerance = 100; // how many millivolts off expected value will we accept?
+	
+	//expected_value_<channel> = expected value in millivolts of regulator on <channel>
+	const uint16_t expected_value_0 = 3600;
+	const uint16_t calibration_0 = 2020;
+	
+	const uint16_t expected_value_1 = 3600; 
+	const uint16_t calibration_1 = 26;
+	
+	const uint16_t expected_value_2 = 5000; 
+	const uint16_t calibration_2 = 3326;
+	
+	const uint16_t expected_value_3 = 3300; 
+	const uint16_t calibration_3 = 2005; 
+	
+	const uint16_t reference_value = 3300 * 1000;
+	const uint16_t total = 4096 * 1000; 
+	
+	uint16_t difference; 
+	
+	setup_pin(true, P_RAD_PWR_RUN); //3.6V regulator 
+	setup_pin(true, P_5V_EN); // 5V regulator 
+	
+		
+	set_output(true, P_RAD_PWR_RUN);		
+	set_output(true, P_5V_EN);
+	
+	AD7991_change_channel(0x0); //Set channel to 0 -> 3.6V Regulator Voltage Sense
+	uint16_t reading = AD7991_read(0x0); 
+	uint32_t hr_read = (reading * reference_value * calibration_0)/total;
+	
+	//compute difference, if you have a better way to get abs difference between 2 
+	//uint16_t without knowing which one is bigger pls email me jtboone12@gmail.com
+	if (reading > expected_value_0) {
+		difference = reading - expected_value_0;
+	} else {
+		difference = expected_value_0 - reading; 
+	}
+	
+	//is the value acceptable? 
+	if (difference > error_tolerance){
+		results[0] = 1; 
+	} else {
+		results[0] = 0; 
+	}
+	
+	AD7991_change_channel(0x1); //Set channel to 1 -> 3.6V Regulator Current Sense
+	reading = AD7991_read(0x1); 
+	reading = (reading * reference_value * calibration_1)/total; //turn reading into millivolts
+	
+	//compute difference, if you have a better way to get abs difference between 2 
+	//uint16_t without knowing which one is bigger pls email me jtboone12@gmail.com
+	if (reading > expected_value_1) {
+		difference = reading - expected_value_1;
+	} else {
+		difference = expected_value_1 - reading; 
+	}
+	
+	//is the value exceptable? 
+	if (difference > error_tolerance){
+		results[1] = 1; 
+	} else {
+		results[1] = 0; 
+	}
+	
+	AD7991_change_channel(0x2); //Set channel to 1 -> 3.6V Regulator Current Sense
+	reading = AD7991_read(0x2); 
+	reading = (reading * reference_value * calibration_2)/total; //turn reading into millivolts
+	
+	//compute difference, if you have a better way to get abs difference between 2 
+	//uint16_t without knowing which one is bigger pls email me jtboone12@gmail.com
+	if (reading > expected_value_2) {
+		difference = reading - expected_value_2;
+	} else {
+		difference = expected_value_2 - reading; 
+	}
+	
+	//is the value exceptable? 
+	if (difference > error_tolerance){
+		results[2] = 1; 
+	} else {
+		results[2] = 0; 
+	}
+	
+	AD7991_change_channel(0x3); //Set channel to 1 -> 3.6V Regulator Current Sense
+	reading = AD7991_read(0x3); 
+	reading = (reading * reference_value * calibration_3)/total; //turn reading into millivolts
+	
+	//compute difference, if you have a better way to get abs difference between 2 
+	//uint16_t without knowing which one is bigger pls email me jtboone12@gmail.com
+	if (reading > expected_value_3) {
+		difference = reading - expected_value_3;
+	} else {
+		difference = expected_value_3 - reading; 
+	}
+	
+	//is the value exceptable? 
+	if (difference > error_tolerance){
+		results[3] = 1; 
+	} else {
+		results[3] = 0; 
+	}		
+}
+
 
 
 void system_test(void){
 	
 	//Test IR sensor
-	float test1 = MLX90614_test(MLX90614_DEFAULT_I2CADDR);
+	////Flight Sensors
+	//#define MLX90614_FLASHPANEL_V6_2_1	0x6C
+	//#define MLX90614_TOPPANEL_V4_2		0x6B
+	//#define MLX90614_ACCESSPANEL_V3_1	0x5C
+	//#define MLX90614_SIDEPANEL_V4_2		0x5D
+	//#define MLX90614_SIDEPANEL_V4_3		0x5F
+	//#define MLX90614_SIDEPANEL_V4_4		0x6D
+	
+	struct return_struct_0 testz; 
+	configure_i2c_master(SERCOM4); 
+	//configure_i2c_standard(SERCOM4); //SERCOM4 -> I2C serial port
+
+	uint8_t results[4]; 
+	AD7991_control_test(results);
+	float test1 = MLX90614_test(MLX90614_FLASHPANEL_V6_2_1);
+	
 	float test2 = AD590_test(); 
 	MPU9250Reading test3 = MPU9250_test();
 	float test4 = HMC5883L_test(); 
