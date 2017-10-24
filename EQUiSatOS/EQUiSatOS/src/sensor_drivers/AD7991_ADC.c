9eb10633e052827dac97c37a56c4fbbd2764681d
JarodBoone <jtboone12@gmail.com>
2017-10-24 17:26:39 -0400

    chaned init function in AD7991 to set all channel bits to 1 and added read function to read from all channel bits

--- EQUiSatOS/EQUiSatOS/src/sensor_drivers/AD7991_ADC.c
+++ EQUiSatOS/EQUiSatOS/src/sensor_drivers/AD7991_ADC.c
@@ -7,45 +7,76 @@
 
 #include "AD7991_ADC.h"
 
-void AD7991_init(){
-	//AD7991_change_channel(0x0);	
+//Make sure I2C is configured (on SERCOM4) 
+enum status_code AD7991_init(){	
+	//(1111 0000) write to AD7991 address to read from all regulators 
+	//read 8 bytes 1-2 -> channel 1, 3-4 -> channel 2 ... etc 
+	uint8_t all_reg = 0xf0;
+	
+	return writeDataToAddress(all_reg, 1, AD7991_ADDR, true);	
 }
 
+enum status_code AD7991_read_all(uint8_t *results){
+	//AD7991_change_channel(channel);
+	uint8_t buffer[8];// = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+	struct return_struct_16 rs;
+	enum status_code read = readFromAddress(buffer,8,AD7991_ADDR,true);
+	results[0] = ((buffer[0] & 0b00001111) << 8) + buffer[1];
+	results[1] = ((buffer[2] & 0b00001111) << 8) + buffer[3];
+	results[2] = ((buffer[4] & 0b00001111) << 8) + buffer[5];
+	results[3] = ((buffer[6] & 0b00001111) << 8) + buffer[7];
+	
+	return read; 
+
+};
+
+
+/////////////////////////////////////////////////////////////////////////////
+/* ONLY USE FUNCTIONS ABOVE THIS LINE *//////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
 //change the channel of the external ADC
-struct return_struct_0 AD7991_change_channel(uint8_t channel){
-	uint8_t target[] = {0b10000000};
-	/*
-	int target[] = {0x0};
+void AD7991_change_channel(uint8_t channel){
+	uint8_t target;//[] = {0b10000};
+	
+	//int target[] = {0x0};
 	switch(channel){
 		case 0x0 :
-			target[0] = 0x1 << 0x4;
+			target = 0x1 << 0x4;
 			break;
 		case 0x1 :
-			target[0] = 0x1 << 0x5;
+			target = 0x1 << 0x5;
 			break;
 		case 0x2 :
-			target[0] = 0x1 << 0x6;
+			target = 0x1 << 0x6;
 			break;
 		case 0x3 :
-			target[0] = 0x1 << 0x7;
+			target = 0x1 << 0x7;
 			break;
 		default:
 			//this is not a valid channel
 			return;
 			
-	}*/
+	}
+	//target = target + 3; //Disables the sample delay and bit trial mechanisms
 	struct return_struct_0 rs;
 	rs.return_status = writeDataToAddress(target, 1, AD7991_ADDR, true);
 	
-	return rs;
+	int a = 10;
+	//return rs;
 }
 
-struct return_struct_16 AD7991_read(uint8_t channel){
+uint16_t AD7991_read(uint8_t channel){
 	//AD7991_change_channel(channel);
 	uint8_t buffer[] = {0x0, 0x0};
 	struct return_struct_16 rs;
-	rs.return_status = readFromAddress(buffer,2,AD7991_ADDR,true);
-	rs.return_value = ((buffer[0] & 0b00001111) << 8) + buffer[1];
+	rs.return_status = readFromAddress(buffer,2,AD7991_ADDR,false);
+	rs.return_value =  ((buffer[0] & 0b00001111) << 8)+ buffer[1];
 
-	return rs;
+
+	int a = 5; 
+	return rs.return_value;
 };
+
+
+
--- EQUiSatOS/EQUiSatOS/src/sensor_drivers/AD7991_ADC.h
+++ EQUiSatOS/EQUiSatOS/src/sensor_drivers/AD7991_ADC.h
@@ -12,11 +12,15 @@
 #include <global.h>
 #include "../processor_drivers/I2C_Commands.h"
 
-#define AD7991_ADDR 0b0101000
+#define AD7991_ADDR 0b0101001 //0x29
 #define AD7991_SHOULD_STOP false
 
-void AD7991_init(void);
-struct return_struct_0 AD7991_change_channel(uint8_t channel);
-struct return_struct_16 AD7991_read(uint8_t channel);
+enum status_code AD7991_init(void);
+enum status_code AD7991_read_all(uint8_t *results);
+
+/* Yucky  *////////////////////////////////////
+void AD7991_change_channel(uint8_t channel);///
+uint16_t AD7991_read(uint8_t channel);/////////
+///////////////////////////////////////////////
 
 #endif /* AD7991_ADC_H_ */
