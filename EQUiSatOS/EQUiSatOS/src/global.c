fb88ae9a71f263193783e4d0f212a8bbb9e9a068
Mckenna Cisler <mckennacisler@gmail.com>
2017-11-22 16:03:27 -0500

    Basic work on Tracelyzer streaming mode

--- EQUiSatOS/EQUiSatOS/src/global.c
+++ EQUiSatOS/EQUiSatOS/src/global.c
@@ -9,8 +9,13 @@
 
 void init_tracelyzer(void) {
 	#if configUSE_TRACE_FACILITY == 1
-	vTraceEnable(TRC_START);
-	global_trace_channel = xTraceRegisterString("Global Events");
+		#if TRC_CFG_RECORDER_MODE == TRC_RECORDER_MODE_STREAMING
+			vTraceEnable(TRC_INIT); // start streaming data when requested instead of waiting here
+		#elif TRC_CFG_RECORDER_MODE == TRC_RECORDER_MODE_SNAPSHOT
+			vTraceEnable(TRC_START);
+		#endif
+	
+		global_trace_channel = xTraceRegisterString("Global Events");
 	#endif
 }
 
@@ -18,7 +23,7 @@ void trace_print(const char *format)
 {
 	// NOTE: va_args won't work; no function that takes that :(
 	#if configUSE_TRACE_FACILITY == 1
-	vTracePrintF(global_trace_channel, format);
+		vTracePrintF(global_trace_channel, format);
 	#endif
 }
 
