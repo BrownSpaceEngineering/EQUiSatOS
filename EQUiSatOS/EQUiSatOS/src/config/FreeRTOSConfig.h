3fb68ad0e0b6865cd64ddec4776973338f947d21
Mckenna Cisler <mckennacisler@gmail.com>
2018-01-22 01:25:30 -0500

    Attempt to fix interrupt priority issues
    Didn't work...

--- EQUiSatOS/EQUiSatOS/src/config/FreeRTOSConfig.h
+++ EQUiSatOS/EQUiSatOS/src/config/FreeRTOSConfig.h
@@ -16,7 +16,7 @@ void assert_triggered( const char * file, uint32_t line );
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_TICK_HOOK                     0
 #define configPRIO_BITS                         2
-#define configCPU_CLOCK_HZ                      ( 8000000 )
+#define configCPU_CLOCK_HZ                      ( 8000000 ) // (system_gclk_gen_get_hz(GCLK_GENERATOR_0))
 #define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 ) // so portTICK_PERIOD_MS = 1
 #define configMAX_PRIORITIES                    ( ( uint32_t ) 5 )
 #define configMINIMAL_STACK_SIZE                ( ( uint16_t ) 100 )
@@ -26,7 +26,7 @@ void assert_triggered( const char * file, uint32_t line );
 #define configSUPPORT_DYNAMIC_ALLOCATION		0
 #define configAPPLICATION_ALLOCATED_HEAP		0 // no need if no heap
 #define configMAX_TASK_NAME_LEN                 ( 8 )
-#define configUSE_TRACE_FACILITY                1			// SET to 1 to use Tracelyzer; 0 to free up the RAM space
+#define configUSE_TRACE_FACILITY                0			// SET to 1 to use Tracelyzer; 0 to free up the RAM space
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
 #define configUSE_MUTEXES                       1
@@ -79,20 +79,25 @@ standard names - or at least those used in the unmodified vector table. */
 #define xPortPendSVHandler                      PendSV_Handler
 #define xPortSysTickHandler                     SysTick_Handler
 
-/* The lowest interrupt priority that can be used in a call to a "set priority"
-function. */
-#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY			0x0f
+/* The lowest (highest-valued) interrupt priority that can be used in a call to 
+a "set priority" function. */
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY			0x3
 
 /* The highest interrupt priority that can be used by any interrupt service
 routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
 INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
-PRIORITY THAN THIS! (higher priorities are lower numeric values. */
-#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY	10
+PRIORITY THAN THIS! (higher priorities are lower numeric values.) */
+// err heavily on the side of safety (over speed) by allowing any interrupt 
+// to use FreeRTOS API functions and therefore not blocking off any interrupts
+// in RTOS critical sections (this helps prevent associated hard faults)
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY	0x0
 
 /* Interrupt priorities used by the kernel port layer itself.  These are generic
 to all Cortex-M ports, and do not rely on any particular library functions. */
-#define configKERNEL_INTERRUPT_PRIORITY 		( configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
+#define CORTEX_M_ISR_PRIORITY_MASK				(0xff >> configPRIO_BITS)
+// (the intent is to set the top configPRIO_BITS to the values, and the rest of the bits to 1)
+#define configKERNEL_INTERRUPT_PRIORITY 		( CORTEX_M_ISR_PRIORITY_MASK | (configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS)) )
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY 	( CORTEX_M_ISR_PRIORITY_MASK | (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS)) )
 
 /* Integrates the Tracealyzer recorder with FreeRTOS */
 #if ( configUSE_TRACE_FACILITY == 1 )
