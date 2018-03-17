8a2616044233b6523fde89ecb7ec76e22d38ec91
Mckenna Cisler <mckennacisler@gmail.com>
2018-03-17 16:34:58 -0400

    Revert to debug config values

--- EQUiSatOS/EQUiSatOS/src/config.h
+++ EQUiSatOS/EQUiSatOS/src/config.h
@@ -21,7 +21,7 @@
  * MAKE SURE to RE-BUILD solution to be double-sure they were disabled
  */
 #define FLASH_ACTIVE // enable flashing
-#define ANTENNA_DEPLOY_ACTIVE // enable antenna deployment
+//#define ANTENNA_DEPLOY_ACTIVE // enable antenna deployment
 #define TRANSMIT_ACTIVE // enable radio transmission (note PRINT_DEBUG must be 0)
 #define BAT_CHARGING_ACTIVE // enable battery charging logic ACTUALLY changing battery actions
 #define WATCHDOG_RESET_ACTIVE // enable watchdog actually rebooting satellite
@@ -31,48 +31,48 @@
 // ***AT THE EXPENSE OF COMPLETELY NORMAL FUNCTIONALITY***
 // ENUMERATION of changes when ON:
 // - don't read data distributed around an orbit
-//#define TESTING_SPEEDUP
-//#define TESTING_SPEEDUP_LONG_OPS
+#define TESTING_SPEEDUP
+#define TESTING_SPEEDUP_LONG_OPS
 
 // reduces the stack size of some tasks to a workable but risky level
 //#define RISKY_STACK_SIZES
 
 // use this flag to disable reed-solomon tables (frees up memory)
-#define USE_REED_SOLOMON
+//#define USE_REED_SOLOMON
 
 // if defined, explicitly sets the initial SAT state (must set both)
-//#define OVERRIDE_INIT_SAT_STATE		INITIAL
-//#define OVERRIDE_INIT_TASK_STATES	INITIAL_TASK_STATES
+#define OVERRIDE_INIT_SAT_STATE		IDLE_NO_FLASH
+#define OVERRIDE_INIT_TASK_STATES	IDLE_NO_FLASH_TASK_STATES
 //													WDOG,  STATE,	(ant),	BAT,	TRANS,	FLASH,	IDLE,  	LOWP,	ATTI,	PERSIST
 //#define OVERRIDE_INIT_TASK_STATES	((task_states){{true,	true,	false,	true,	false,	false,	false,	false,	true,   true}})
 #define OVERRIDE_STATE_HOLD_INIT	0 // whether to hold initial state (stop auto state changes)
 
 // whether to start up misc. testing tasks (doing both at same time will likely run out of mem)
 //#define ONLY_RUN_TESTING_TASKS	// doesn't run normal EQUISAT tasks
-//#define RUN_RTOS_SYSTEM_TEST	// ~800 bytes of memory
+#define RUN_RTOS_SYSTEM_TEST	// ~800 bytes of memory
 	// less data
 	//#define RTOS_SYSTEM_TEST_SUMMARY
 	// whether to print all data every test or only one element per stack (with exception of error stack)
 	//#define RTOS_SYSTEM_TEST_ONLY_RECENT
-//#define SYSTEM_TEST_TASK_FREQ		(43*1000) // ms
+	#define SYSTEM_TEST_TASK_FREQ		(43*1000) // ms
 //#define RUN_TESTING_TASK		// ~1024 bytes of memory
 //#define RUN_TASK_STACK_TESTS
 
 // whether to rewrite MRAM with "zero" values (done before launch)
-//#define WRITE_DEFAULT_MRAM_VALS
+#define WRITE_DEFAULT_MRAM_VALS
 
 /** System Tests/Simulations **/
 //#define EQUISIM_SIMULATE_BATTERIES // see config in equisim_simulated_data.h
-//#define EQUISIM_SIMULATE_DIRECT_STATE_CHANGES // must disable OVERRIDE_STATE_HOLD_INIT!
-//	#define EQUISIM_IN_STATE_TIME_MS			(1*60*1000)
+#define EQUISIM_SIMULATE_DIRECT_STATE_CHANGES // must disable OVERRIDE_STATE_HOLD_INIT!
+	#define EQUISIM_IN_STATE_TIME_MS			(1*60*1000)
 //#define EQUISIM_WATCHDOG_RESET_TEST
 
 /** Debug **/
 // whether to include Tracelyzer tracing library
-#define USE_TRACELYZER				0
+#define USE_TRACELYZER				1
 
 // whether to configASSERTs (and other asserts) should hang
-//#define USE_ASSERTIONS
+#define USE_ASSERTIONS
 
 // add strict asserts to notify of events that we want to be rare
 //#define USE_STRICT_ASSERTIONS
@@ -82,16 +82,16 @@
 // 1 to print over USART
 // 2 to print over Tracelyzer trace_print
 // 3 to print over both
-#define PRINT_DEBUG					0
+#define PRINT_DEBUG					1
 // whether to let normal transmissions print while print enabled
-//#define DONT_PRINT_RAW_TRANSMISSIONS
+#define DONT_PRINT_RAW_TRANSMISSIONS
 // whether to use a mutex to avoid collisions with radio
 // define for radio testing but turn off for timing/task testing
 // (it slows down and alters the OS timings)
 //#define SAFE_PRINT	
 
 // define to print errors as the come in
-//#define PRINT_NEW_ERRORS // note: probably will result in many stack overflows
+#define PRINT_NEW_ERRORS // note: probably will result in many stack overflows
 
 /****************************************************************************************/
 /*  DO NOT EDIT BELOW THIS LINE UNLESS YOU ARE INTENTIONALLY MODIFYING CONFIGURATION    */
